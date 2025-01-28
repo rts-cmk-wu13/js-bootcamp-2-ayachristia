04a255b73b49bc2a3cb8b3fa07d0298ab7c397aa
iamayac <ayachristia@gmail.com>
2025-01-28 15:17:31 +0100

    ongoing

---
+++ Opgaver/Niveau1/01/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// ------1
+const button = document.querySelector("#my-button");
+console.log(button);
+
+button.addEventListener("click", function () {
+  button.classList.toggle("toggle-color");
+});
+
+// ------2
+function beregnForhold(a, b) {
+  let result = a / b;
+  return Math.floor(result);
+}
+
+console.log(beregnForhold(4, 3));
+
+// -ekstra opgave
+
+function beregnForhold2(a, b) {
+  if (b === 0) {
+    return null;
+  } else {
+    let result = a % b;
+    return result;
+  }
+}
+const result2 = beregnForhold2(4, 0);
+console.log(result2);
+
+// -------3
+
+function skjulElementViaCssSelector(cssSelector) {
+  const element = document.querySelector(cssSelector);
+  if (element) {
+    element.style.display = "none";
+  }
+}
+
+skjulElementViaCssSelector("p");
