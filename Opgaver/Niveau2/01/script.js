2d5246a17f2c461419d7737162c223a8ce117c8b
iamayac <ayachristia@gmail.com>
2025-02-01 11:43:00 +0100

    assignment finished

--- Opgaver/Niveau2/01/script.js
+++ Opgaver/Niveau2/01/script.js
@@ -52,6 +52,73 @@ buttons.forEach((btn) => {
     btn.classList.remove("btnNeonClicked");
   });
 });
+
+// ---------------- 2 plus/minus region (cloneNode)
+//hver gang man modificere innerHTML skal browseren genlæse og rekonstruere DOM elementet hvilket kan gøre load langsommere for større product databaser, derfor er en dynamisk template at foretrække ved større vare kartoteker
+
+// array til at tilføje produkter dynamisk
+const products = [
+  {
+    name: "1460 Pascal max-platformstøvler i læder",
+    price: 2100,
+    image: "img/Skærmbillede 2025-01-29 093309.jpg",
+    amount: 0,
+  },
+  {
+    name: "Dr. Martens 1460 Støvler",
+    price: 1600,
+    image: "img/bex.jpg",
+    amount: 0,
+  },
+  {
+    name: "Chelsea Støvler",
+    price: 1800,
+    image: "img/regular.jpg",
+    amount: 0,
+  },
+];
+
+const productsTemplate = document.querySelector(".product_template");
+const productsContainerEl = document.querySelector(".products");
+
+// for hvert produkt i array skabes en variabel der modtager en function som returnere produktet i en produkt template
+products.forEach((product) => {
+  const productEl = createProduct(product);
+  console.log(productEl);
+
+  productsContainerEl.appendChild(productEl);
+});
+
+function createProduct(product) {
+  const productClone = productsTemplate.content.cloneNode(true);
+
+  const productName = productClone.querySelector(".product__name");
+  const productPrice = productClone.querySelector(".product__price");
+  const productImg = productClone.querySelector(".product__img");
+  const productMinusBtn = productClone.querySelector(".product__arrow--left");
+  const productPlusBtn = productClone.querySelector(".product__arrow--right");
+  const productCount = productClone.querySelector(".product__count");
+
+  productName.textContent = product.name;
+  productPrice.textContent = product.price;
+  productImg.textContent = product.img;
+  productImg.alt = product.name;
+  productCount.textContent = product.amount;
+
+  productMinusBtn.addEventListener("click", () => {
+    if (product.amount > 0) {
+      product.amount--;
+      productCount.textContent = product.amount;
+    }
+  });
+
+  productPlusBtn.addEventListener("click", () => {
+    product.amount++;
+    productCount.textContent = product.amount;
+  });
+
+  return productClone;
+}
 //------------------ 2 plus /minus basic
 // const products = [
 //   {
@@ -122,70 +189,3 @@ buttons.forEach((btn) => {
 //     }
 //   });
 // });
-
-// ---------------- 2 plus/minus region (cloneNode)
-//hver gang man modificere innerHTML skal browseren genlæse og rekonstruere DOM elementet hvilket kan gøre load langsommere for større product databaser
-
-// array til at tilføje produkter dynamisk
-const products = [
-  {
-    name: "1460 Pascal max-platformstøvler i læder",
-    price: 2100,
-    image: "img/Skærmbillede 2025-01-29 093309.jpg", // Opdateret sti
-    amount: 0,
-  },
-  {
-    name: "Dr. Martens 1460 Støvler",
-    price: 1600,
-    image: "img/bex.jpg", // Andet billede
-    amount: 0,
-  },
-  {
-    name: "Chelsea Støvler",
-    price: 1800,
-    image: "img/regular.jpg", // Tredje billede
-    amount: 0,
-  },
-];
-
-const productsTemplate = document.querySelector(".product_template");
-const productsContainerEl = document.querySelector(".products");
-
-// for hvert produkt i array skabes en variabel der modtaget en function som returnere produktet i en produkt template
-products.forEach((product) => {
-  const productEl = createProduct(product);
-  console.log(productEl);
-  //dernæst sættes dette produkt på containeren
-  productsContainerEl.appendChild(productEl);
-});
-
-function createProduct(product) {
-  const productClone = productsTemplate.content.cloneNode(true);
-
-  const productName = productClone.querySelector(".product__name");
-  const productPrice = productClone.querySelector(".product__price");
-  const productImg = productClone.querySelector(".product__img");
-  const productMinusBtn = productClone.querySelector(".product__arrow--left");
-  const productPlusBtn = productClone.querySelector(".product__arrow--right");
-  const productCount = productClone.querySelector(".product__count");
-
-  productName.textContent = product.name;
-  productPrice.textContent = product.price;
-  productImg.textContent = product.img;
-  productImg.alt = product.name;
-  productCount.textContent = product.amount;
-
-  productMinusBtn.addEventListener("click", () => {
-    if (product.amount > 0) {
-      product.amount--;
-      productCount.textContent = product.amount;
-    }
-  });
-
-  productPlusBtn.addEventListener("click", () => {
-    product.amount++;
-    productCount.textContent = product.amount;
-  });
-
-  return productClone;
-}
